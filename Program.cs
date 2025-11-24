86469f7ac8ff698bedcbc867d57625b602cc5be6
Igor Brito <gestorigorbrito@gmail.com>
2025-11-24 12:17:57 -0300

    continuidade personagens

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
+using System.Reflection.PortableExecutable;
+
 namespace TesteVillelaRPG
 {
     public class Personagem 
@@ -8,4 +11,38 @@ namespace TesteVillelaRPG
         public int Nivel { get; set; }
         public int atack { get; set; }
     }
-}
+    
+    class program
+    {
+        static string gameState = "selection"
+        static Personagem player = null;
+        static Personagem enemy = null;
+
+        static void main(string[] args)
+        {
+            while (true)
+            {
+                if (gameState == "selection")
+                {
+                    CharacterSelection();
+                }
+                else if (gameState == "battle" && player != null && enemy != null)
+                {
+                    BattleArena();
+                }
+            }
+        }
+
+        static void CharacterSelection()
+        { }
+            Console.Clear();
+            Console.WriteLine("=== SELEÇÃO DE PERSONAGENS ===");
+
+            var personagens = new List<Personagem>()
+            {
+                new Personagem { Nome = "Guerreiro", Vida = 100, Ataque = 20 },
+                new Personagem { Nome = "Mago", Vida = 70, Ataque = 30 },
+                new Personagem { Nome = "Arqueiro", Vida = 80, Ataque = 25 }
+            };
+
+          
